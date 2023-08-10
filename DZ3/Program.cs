f2275039c97204ef574d8b239a903a7bae95ff59
Maikl <umnov.mixail@mail.ru>
2023-08-10 12:38:00 +0400

    acdc

---
+++ DZ3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2; b = 10 -> max = 10
+// a = -9; b = -3 -> max = -3
+
+System.Console.WriteLine("Напишите 1 число");
+int num1 = Console.ReadLine.ToInt32();
+System.Console.WriteLine("Напишите 2 число");
+int num2 = Console.ReadLine.ToInt32();
+int Max = 0;
+if(num1<num2=Max==num2||num1>num2=Max==num1)
+{
+    System.Console.WriteLine(Max);
+}
